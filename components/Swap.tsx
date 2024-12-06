a213b419403004ad6bbb6a6911cf08b3de12f3c7
Mike Solomon <mike.solomon@coinbase.com>
2024-12-05 19:37:53 -0500

    swappable tokens

--- components/Swap.tsx
+++ components/Swap.tsx
@@ -5,11 +5,31 @@ import sdk, {
   type FrameContext,
 } from "@farcaster/frame-sdk";
 import { SwapDefault } from "@coinbase/onchainkit/swap";
+import { Token } from "@coinbase/onchainkit/token";
 
 export default function Swap() {
   const [isSDKLoaded, setIsSDKLoaded] = useState(false);
   const [, setContext] = useState<FrameContext>();
 
+  const ETHToken: Token = {
+    address: "",
+    chainId: 8453,
+    decimals: 18,
+    name: "Ethereum",
+    symbol: "ETH",
+    image: "",
+  };
+
+  const USDCToken: Token = {
+    address: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+    chainId: 8453,
+    decimals: 6,
+    name: "USDC",
+    symbol: "USDC",
+    image: "",
+  };
+
+  const swappableTokens: Token[] = [ETHToken, USDCToken];
   useEffect(() => {
     const load = async () => {
       setContext(await sdk.context);
@@ -27,7 +47,7 @@ export default function Swap() {
 
   return (
     <div className="max-w-3xl mx-auto py-4 px-2">
-      <SwapDefault to={[]} from={[]} />
+      <SwapDefault to={swappableTokens} from={swappableTokens} />
     </div>
   );
 }
