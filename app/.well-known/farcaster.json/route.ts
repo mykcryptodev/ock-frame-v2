b3bd739f0b743deda208edb50053b44fd4460a62
Mike Solomon <mike.solomon@coinbase.com>
2024-12-05 19:24:06 -0500

    signed manifest

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -3,11 +3,9 @@ export async function GET() {
 
   const config = {
     accountAssociation: {
-      header:
-        "eyJmaWQiOjM2MjEsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgyY2Q4NWEwOTMyNjFmNTkyNzA4MDRBNkVBNjk3Q2VBNENlQkVjYWZFIn0",
-      payload: "eyJkb21haW4iOiJmcmFtZXMtdjIudmVyY2VsLmFwcCJ9",
-      signature:
-        "MHhiNDIwMzQ1MGZkNzgzYTExZjRiOTllZTFlYjA3NmMwOTdjM2JkOTY1NGM2ODZjYjkyZTAyMzk2Y2Q0YjU2MWY1MjY5NjI5ZGQ5NTliYjU0YzEwOGI4OGVmNjdjMTVlZTdjZDc2YTRiMGU5NzkzNzA3YzkxYzFkOWFjNTg0YmQzNjFi",
+      header: "eyJmaWQiOjIxNzI0OCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweGViYTc4NzE3YjZmMDU5Q0ZFMGI3NUU3NUMyZWQ0QkI3Y0E2NTE1NEYifQ",
+      payload: "eyJkb21haW4iOiJvY2stZnJhbWUtdjIudmVyY2VsLmFwcCJ9",
+      signature: "MHhiZTlmZGNkNWQzZDMyMDBhZDlmMjAzYjA1NTBiY2M3YjA4ZDkyZjMyZjg0MzYwYTcxNDNkZWQyMzUyNzJjZDk4NDcxNzE3ZTY4ZTI0MmRiMDgyNDUyZjdkZjY5YzYyZjM4YmYxZDhmMjM2NzBiYjI3NTZmYWMxOTI2N2IwNjBiNjFj" 
     },
     frame: {
       version: "0.0.0",
